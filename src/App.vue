0c7e4a082103fdf4dc0e02bfdcdbf03acd331fa6
welldy33 <diezer33@gmail.com>
2020-09-19 00:33:22 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       items: [
           { age: 40, first_name: 'Dickerson', last_name: 'Macdonald',_rowVariant: 'danger' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw', _cellVariants: { age: 'success', first_name: 'warning' } },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw', _cellVariants: { age: 'info', first_name: 'warning' } },
           { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
           { age: 38, first_name: 'Jami', last_name: 'Carney' }
         ]
